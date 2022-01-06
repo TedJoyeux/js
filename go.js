0d6b890dc8a3c95e1ec4673d983f81717ccd286e
TedJoyeux <impundujoyeuxthevenin@gmail.com>
2022-01-06 07:33:41 +0100

    chalenge

---
+++ go.js
@@ -0,0 +1,19 @@
+
+
+
+function changeCar(phrase, old, news, debut, fin){
+    this.phrase = phrase;
+    this.old= old;
+    this.new = news;
+    this.debut = debut;
+    this.fin = fin;
+   
+    
+}
+const phrase = new changeCar(" I have many cars my old is pink colour ","null", "null");
+console.log(phrase);
+console.log(changeCar(phrase,' ', '*', null, null));
+
+
+
+
